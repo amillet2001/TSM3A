d913403e0d7dadde7778d70173076115d675a62b
Tadaomi05 <159202994+Tadaomi05@users.noreply.github.com>
2024-02-07 10:21:39 +0900

    Update Calculator.java
    test

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class Calculator 
+public class Calculator    
 {
 
     public static void main(String[] args) 
